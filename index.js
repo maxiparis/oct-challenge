be655162728e7fd02f62ad2bc5881adfc3eec707
Maximiliano Paris <mparis@hotmail.cl>
2024-10-04 20:52:48 -0600

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", handleClick);
+
+function handleClick() {
+    const userInput = document.getElementById("userInput").value;
+    console.log(`userInput = ${userInput}`);
+    document.getElementById("count").innerHTML = computeDivisorsOfThree(userInput.split(",")).toString();
+}
+
+function computeDivisorsOfThree(array) {
+    let count = 0;
+    if (Array.isArray(array)) {
+        for (let i = 0; i < array.length; i++) {
+            if (typeof array[i] == "number" && array[i] % 3 === 0) {
+                count++;
+            } else {
+                alert("Enter numbers");
+            }
+        }
+        return count;
+    } else {
+        window.alert("Enter a valid list of numbers");
+        return 0;
+    }
+}
